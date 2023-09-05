e1b0dc6a6fbc90c96edf7a2d106336572116e255
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-09-05 10:41:08 +0800

    [nemu]: open difftest

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -13,12 +13,41 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#include <isa.h>
-#include <cpu/difftest.h>
 #include "../local-include/reg.h"
+#include "common.h"
+#include <cpu/difftest.h>
+#include <isa.h>
+
+#define FOREACH_REGS                                                           \
+  for (int i = 0; i < 32; i++) {                                               \
+    LAMBDA(reg_name(i), ref_r->gpr[i], cpu.gpr[i]);                            \
+  }                                                                            \
+  LAMBDA("pc", ref_r->pc, cpu.pc);
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  bool same = true;
+#define LAMBDA(name, ref, dut) same &= ref == dut
+  FOREACH_REGS
+#undef LAMBDA
+  return same;
+}
+
+static void showErrorReg(const char *name, word_t ref, word_t dut) {
+#define FMT_REG "[%s] = " FMT_WORD ", " DEC_WORD
+  bool error = ref != dut;
+  const char *dutFmt = error ? ANSI_FMT(FMT_REG, ANSI_FG_RED) : FMT_REG;
+  if (error) {
+    printf(dutFmt, name, dut, dut);
+    printf(ANSI_FMT(FMT_REG, ANSI_FG_GREEN), name, ref, ref);
+  }
+#undef FMT_REG
+}
+
+void isa_difftest_showError(CPU_state *ref_r, vaddr_t pc) {
+  error("Difftest cache Nemu error at PC = " FMT_WORD "\n", pc);
+#define LAMBDA(name, ref, dut) showErrorReg(name, ref, dut)
+  FOREACH_REGS
+#undef LAMBDA
 }
 
 void isa_difftest_attach() {
