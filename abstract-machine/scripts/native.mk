ef22f1bf5c468d44360922cf283e8f5f1f58c723
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-09-06 16:51:27 +0800

    [am]: add gdb -g -Og for debug

--- abstract-machine/scripts/native.mk
+++ abstract-machine/scripts/native.mk
@@ -18,7 +18,7 @@ LDFLAGS_CXX = $(addprefix -Wl$(comma), $(LDFLAGS))
 
 image:
 	@echo + LD "->" $(IMAGE_REL)
-	@g++ -pie -o $(IMAGE) -Wl,--whole-archive $(LINKAGE) -Wl,-no-whole-archive $(LDFLAGS_CXX) -lSDL2 -ldl
+	@g++ -pie -o $(IMAGE) -Wl,--whole-archive $(LINKAGE) -Wl,-no-whole-archive $(LDFLAGS_CXX) -lSDL2 -ldl -g
 
 run: image
 	$(IMAGE)
