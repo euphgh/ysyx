3c14c9a8fd4f97c04a699b929979b9cc09a2c6dd
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-09-12 18:37:59 +0800

    [nemu]: rewrite csr generate scripts

--- nemu/tools/csr.py
+++ nemu/tools/csr.py
@@ -2,62 +2,63 @@ from enum import auto, Enum
 from pdb import set_trace
 
 WORD_LEN = 64
-RONL = auto()
-WPRI = auto()
-WLRL = auto()  # not legal value will not write
-WARL = auto()  # not legal value will not write
 
-class CSR(Enum):
-    NAME = 0
-    NUM = 1
-    FIELDS = 2
-    PRI = 3
+class FieldSpec(Enum):
+    READ = auto()
+    WPRI = auto()
+    WLRL = auto()
+    WARL = auto()
 
-class FIELD(Enum):
-    NAME = 0
-    MSB = 1
-    LSB = 2
-    INIT = 3
-    WATTR = 4
-    LEG_FUNC = 5
 
-AllCRS = [
-    (
-        "mtvec",
-        0x305,
-        [
-            ("base", WORD_LEN - 1, 2, WARL),
-            ("mode", 1, 0, WARL),
-        ],
-    ),
-    (
-        "mepc",
-        0x341,
-        [
-            ("all", WORD_LEN - 1, 0, WARL),
-        ],
-    ),
-    (
-        "mcause",
-        0x342,
-        [
-            ("interrupted", WORD_LEN - 1, WORD_LEN - 1, RONL),
-            ("exceptionCode", WORD_LEN - 2, 0, WLRL),
-        ],
-    ),
-    (
-        "mstatus",
-        0x300,
-        [
-            ("mie", 3, 3, WARL),
-            ("mpie", 7, 7, WARL),
-            ("mpp", 12, 11, WARL),
-        ],
-    ),
-]
+class Field:
+    def __init__(
+        self,
+        name: str,
+        msb: int,
+        lsb: int,
+        spec: FieldSpec,
+        init: int,
+        legalFunc: str,
+    ) -> None:
+        self.name = name
+        self.msb = msb
+        self.lsb = lsb
+        self.spec = spec
+        self.init = init
+        self.legalFunc = legalFunc
 
-headCode = []
-srcCode = []
+    def __str__(self) -> str:
+        return "({:s} [{:d}, {:d}] = {:d}, {:s})".format(
+            self.name,
+            self.msb,
+            self.lsb,
+            self.init,
+            self.spec,
+        )
+
+    def structDef(self) -> str:
+        return "word_t {:s}: {:d};".format(self.name, self.msb - self.lsb + 1)
+
+    def writeLine(self, csrName) -> str:
+        return (
+            (
+                (
+                    "if ({func:s}(newVar.{fieldName:s})) {{ {csrName:s}.{fieldName:s} = newVar.{fieldName:s}; }}".format(
+                        func=self.legalFunc, fieldName=self.name, csrName=csrName
+                    )
+                    if (self.legalFunc != None)
+                    else "{csrName:s}.{fieldName:s} = newVar.{fieldName:s};".format(
+                        fieldName=self.name, csrName=csrName
+                    )
+                )
+            )
+            if (self.spec == FieldSpec.WARL) or (self.spec == FieldSpec.WLRL)
+            else ""
+        )
+
+    def initValue(self) -> int:
+        return self.init << self.lsb
+
 
 srcVarDef = "{name:s}_t {name:s};"
 headTypeDef = (
@@ -68,19 +69,9 @@ typedef union {{
     }};
     word_t val;
 }} {name:s}_t;
-extern 
-"""
+extern"""
     + srcVarDef
 )
-
-csrWriteFmt = """
-static bool {csrName:s}Write(word_t val) {{
-    {csrName:s}_t newVar = {{.val = val}};
-    {code:s}
-}}
-"""
-
-
 csrOpFunc = "bool {name:s}RW(word_t* rd, word_t src1, csrOp op)"
 csrOpDefFmt = csrOpFunc + ";"
 csrOpImpFmt = (
@@ -110,98 +101,162 @@ csrrwImpFmt = (
 }}"""
 )
 
+csrInitFunc = "void csrInit()"
+csrInitDefFmt = csrInitFunc + ";"
+csrInitImpFmt = csrInitFunc + """{{\n{code:s}\n}}"""
 
-def genDefTypeLine(name, width):
-    return "word_t {:s}: {:d};".format(name, width)
 
+class CtrlStatReg:
+    def __init__(self, name: str, num: int, fields: list[Field]) -> None:
+        self.name = name
+        self.num = num
+        self.fields: list[Field] = []
 
-def genWriteFuncLine(field: tuple):
-    return (
-        (
-            (
-                "if ({func:s}(newVar.{fieldName:s})) {{ {csrName:s}.{fieldName:s} = newVar.{fieldName:s}; }}".format(
-                    func=field[FIELD.LEG_FUNC], fieldName=field[FIELD.NAME], csrName=csr[CSR.NAME]
+        bitsList = [-1 for i in range(WORD_LEN + 1)]
+        bitsList[WORD_LEN] = -2
+        for idx in range(len(fields)):
+            field = fields[idx]
+            for bits in range(field.lsb, field.msb + 1):
+                bitsList[bits] = idx
+
+        startBits = 0
+        for i in range(1, WORD_LEN + 1):
+            if bitsList[i - 1] != bitsList[i]:
+                endBits = i - 1
+                self.fields.append(
+                    Field(
+                        "r{:d}to{:d}".format(endBits, startBits),
+                        endBits,
+                        startBits,
+                        FieldSpec["READ"],
+                        0,
+                        None,
+                    )
+                    if (bitsList[endBits] < 0)
+                    else fields[bitsList[endBits]]
                 )
-                if (len(field) == len(FIELD))
-                else "{csrName:s}.{fieldName:s} = newVar.{fieldName:s};".format(
-                    fieldName=field[FIELD.NAME], csrName=csr[CSR.NAME]
-                )
-            )
+                startBits = i
+
+    def __str__(self) -> str:
+        return "name = {:s}\nnum = 0x{:x}\nfields = {{\n{:s}\n}}".format(
+            self.name,
+            self.num,
+            "\n".join([field.__str__() for field in self.fields]),
         )
-        if (field[FIELD.WATTR] == WLRL) or (field[FIELD.WATTR] == WARL)
-        else ""
-    )
+
+    def genTypeDef(self) -> str:
+        structList = [field.structDef() for field in self.fields]
+        return headTypeDef.format(name=self.name, fields="\n".join(structList))
+        
+
+    def genOpFuncDef(self) -> str:
+        return csrOpDefFmt.format(name=self.name)
+
+    def genOpFuncImp(self) -> str:
+        return csrOpImpFmt.format(name=self.name)
+
+    def genRWcase(self) -> str:
+        return "case 0x{:x}: res = {:s}rw(rd, src1, op); break;".format(
+            self.num, self.name
+        )
+
+    def genPrivateWriteFunc(self) -> str:
+        csrWriteFmt = """
+        static bool {csrName:s}Write(word_t val) {{
+            {csrName:s}_t newVar = {{.val = val}};
+            {code:s}
+        }}"""
+        writeLines = [field.writeLine(self.name) for field in self.fields]
+        return csrWriteFmt.format(csrName=self.name, code="\n".join(writeLines))
+
+    def genInitCode(self) -> str:
+        initValue = 0
+        for field in self.fields:
+            initValue = initValue | field.initValue()
+        return "{:s}.val = {:x};".format(self.name, initValue)
 
 
-csrrwInner = []
+class PaserCSR:
+    def __init__(self, csrs: list[CtrlStatReg]) -> None:
+        self.allCSR = csrs
 
-for csr in AllCRS:
-    defTypeInner = []
-    writeInner = []
-    fillList = [-1 for i in range(WORD_LEN + 1)]
-    fillList[WORD_LEN] = -2
+    def headCode(self) -> str:
+        code = []
+        code = code + [csr.genTypeDef() for csr in self.allCSR]
+        code = code + [csr.genOpFuncDef() for csr in self.allCSR]
+        code.append(csrrwDefFmt)
+        code.append(csrInitDefFmt)
+        return "\n".join(code)
 
-    for idx in range(len(csr[CSR.FIELDS])):
-        field = csr[CSR.FIELDS][idx]
-        for bits in range(field[FIELD.LSB], field[FIELD.MSB] + 1):
-            fillList[bits] = idx
+    def srcCode(self) -> str:
+        code = []
+        code = code + [csr.genPrivateWriteFunc() for csr in self.allCSR]
+        code = code + [csr.genOpFuncImp() for csr in self.allCSR]
+        caseCode = [csr.genRWcase() for csr in self.allCSR]
+        code.append(csrrwImpFmt.format(code="\n".join(caseCode)))
+        initCode = [csr.genInitCode() for csr in self.allCSR]
+        code.append(csrInitImpFmt.format(code="\n".join(initCode)))
+        return "\n".join(code)
 
-    startBits = 0
-    for i in range(1, WORD_LEN + 1):
-        if fillList[i - 1] != fillList[i]:
-            endBits = i - 1
-            field = (
-                ("r{:d}to{:d}".format(endBits, startBits), endBits, startBits, RONL)
-                if (fillList[endBits] < 0)
-                else csr[CSR.FIELDS][fillList[endBits]]
-            )
-            defTypeInner.append(genDefTypeLine(field[FIELD.NAME], endBits - startBits + 1))
-            writeInner.append(genWriteFuncLine(field))
-            print("gen field", field)
-            startBits = i
 
-    # for field in csr[2]:
-    #     defTypeInner.append(
-    #         "word_t {:s}: {:d};".format(field[0], field[1] - field[2] + 1)
-    #     )
-    #     writeLine = (
-    #         (
-    #             (
-    #                 "if ({func:s}(newVar.{fieldName:s})) {{ {csrName:s}.{fieldName:s} = newVar.{fieldName:s}; }}".format(
-    #                     func=field[4], fieldName=field[0], csrName=csr[0]
-    #                 )
-    #                 if (len(field) == 5)
-    #                 else "{csrName:s}.{fieldName:s} = newVar.{fieldName:s};".format(
-    #                     fieldName=field[0], csrName=csr[0]
-    #                 )
-    #             )
-    #         )
-    #         if (field[3] == WLRL) or (field[3] == WARL)
-    #         else ""
-    #     )
-    #     writeInner.append(writeLine)
+def FLD(*args, **kwargs) -> Field:
+    init = 0x0
+    legalFunc = None
+    if len(args) > 4:
+        if isinstance(args[4], int):
+            init = args[4]
+        elif isinstance(args[4], str):
+            legalFunc = args[4]
+        if len(args) > 5:
+            if isinstance(args[4], int):
+                init = args[5]
+            elif isinstance(args[5], str):
+                legalFunc = args[5]
+    return Field(args[0], args[1], args[2], FieldSpec[args[3]], init, legalFunc)
 
-    # define code
-    headCode.append(headTypeDef.format(name=csr[CSR.NAME], fields="\n".join(defTypeInner)))
-    srcCode.append(srcVarDef.format(name=csr[CSR.NAME]))
 
-    srcCode.append(csrWriteFmt.format(csrName=csr[CSR.NAME], code="\n".join(writeInner)))
+def CSR(*args, **kwargs) -> CtrlStatReg:
+    return CtrlStatReg(args[0], args[1], args[2])
 
-    headCode.append(csrOpDefFmt.format(name=csr[CSR.NAME]))
-    srcCode.append(csrOpImpFmt.format(name=csr[CSR.NAME]))
 
-    csrrwInner.append(
-        "case 0x{:x}: res = {:s}rw(rd, src1, op); break;".format(csr[CSR.NUM], csr[CSR.NAME])
-    )
+AllCRS = [
+    CSR(
+        "mtvec",
+        0x305,
+        [
+            FLD("base", WORD_LEN - 1, 2, "WARL"),
+            FLD("mode", 1, 0, "WARL"),
+        ],
+    ),
+    CSR(
+        "mepc",
+        0x341,
+        [
+            FLD("all", WORD_LEN - 1, 0, "WARL"),
+        ],
+    ),
+    CSR(
+        "mcause",
+        0x342,
+        [
+            FLD("interrupted", WORD_LEN - 1, WORD_LEN - 1, "READ"),
+            FLD("exceptionCode", WORD_LEN - 2, 0, "WLRL"),
+        ],
+    ),
+    CSR(
+        "mstatus",
+        0x300,
+        [
+            FLD("mie", 3, 3, "WARL"),
+            FLD("mpie", 7, 7, "WARL"),
+            FLD("mpp", 12, 11, "WARL"),
+        ],
+    ),
+]
 
-headCode.append(csrrwDefFmt)
-srcCode.append(csrrwImpFmt.format(code="\n".join(csrrwInner)))
-
-print("\n".join(headCode))
-print("\n".join(srcCode))
-
-# with open("./src/isa/riscv32/local-include/csrDefine.h", "w") as headFile:
-#     headFile.write("\n".join(headCode))
-
-# with open("./src/isa/riscv32/local-include/csrImplement.h", "w") as srcFile:
-#     srcFile.write("\n".join(srcCode))
+if __name__ == "__main__":
+    paser = PaserCSR(AllCRS)
+    print("// ======================= Head File ========================")
+    print(paser.headCode())
+    print("// ======================= src File ========================")
+    print(paser.srcCode())
