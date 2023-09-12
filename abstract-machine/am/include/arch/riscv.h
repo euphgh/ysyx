3ef5250130ba6d446407b5c25a4b312e6c6feae3
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-09-12 21:35:46 +0800

    [nanos]: add EVENT_YIELD

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -8,10 +8,35 @@
 #endif
 
 struct Context {
-  // TODO: fix the order of these members to match trap.S
-  uintptr_t mepc, mcause, gpr[NR_REGS], mstatus;
-  void *pdir;
+  uintptr_t gpr[NR_REGS], mcause, mstatus, mepc;
 };
+
+#define DEFEXC(int, exc) ((int << (64 - 1)) | exc)
+
+typedef enum {
+  EC_SSoftInt = DEFEXC(1, 1),
+  EC_MSoftInt = DEFEXC(1, 3),
+  EC_STimeInt = DEFEXC(1, 5),
+  EC_MTimeInt = DEFEXC(1, 7),
+  EC_SExternInt = DEFEXC(1, 9),
+  EC_MExternInt = DEFEXC(1, 11),
+  EC_InstrAddrMisAlign = DEFEXC(0, 0),
+  EC_InstrAccessFault = DEFEXC(0, 1),
+  EC_IllegalInstr = DEFEXC(0, 2),
+  EC_BreakPoint = DEFEXC(0, 3),
+  EC_LoadAddrMisAlign = DEFEXC(0, 4),
+  EC_LoadAccessFault = DEFEXC(0, 5),
+  EC_StoreAddrMisAlign = DEFEXC(0, 6),
+  EC_StoreAccessFault = DEFEXC(0, 7),
+  EC_EnvCallFromU = DEFEXC(0, 8),
+  EC_EnvCallFromS = DEFEXC(0, 9),
+  EC_EnvCallFromM = DEFEXC(0, 11),
+  EC_InstrPageFault = DEFEXC(0, 12),
+  EC_LoadPageFault = DEFEXC(0, 13),
+  EC_StorePageFault = DEFEXC(0, 15),
+} ExcCode;
+
+#undef DEFEXC
 
 #ifdef __riscv_e
 #define GPR1 gpr[15] // a5
--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,7 +8,12 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      default: ev.event = EVENT_ERROR; break;
+    case EC_EnvCallFromM:
+      ev.event = EVENT_YIELD;
+      break;
+    default:
+      ev.event = EVENT_ERROR;
+      break;
     }
 
     c = user_handler(ev, c);
