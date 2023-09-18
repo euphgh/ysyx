ae49629825705ec99d2366711481963d6ab31abd
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-09-19 00:48:41 +0800

    [am]: fix am keyboard up down error

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,8 +5,6 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t am_scancode = inl(KBD_ADDR);
-  kbd->keydown = am_scancode >> 31;
+  kbd->keydown = ((am_scancode & KEYDOWN_MASK) != 0);
   kbd->keycode = am_scancode & (~KEYDOWN_MASK);
-  // kbd->keydown = 0;
-  // kbd->keycode = AM_KEY_NONE;
 }
