ba22bf8a64358448ccf173b48586df09a2a0d0f5
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-09-18 21:49:35 +0800

    [navy]: add fs syscall

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -62,8 +62,7 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  _exit(SYS_open);
-  return 0;
+  return _syscall_(SYS_open, (uintptr_t)path, flags, mode);
 }
 
 int _write(int fd, void *buf, size_t count) {
@@ -89,18 +88,13 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  _exit(SYS_read);
-  return 0;
+  return _syscall_(SYS_read, fd, (uintptr_t)buf, count);
 }
 
-int _close(int fd) {
-  _exit(SYS_close);
-  return 0;
-}
+int _close(int fd) { return _syscall_(SYS_close, fd, 0, 0); }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _exit(SYS_lseek);
-  return 0;
+  return _syscall_(SYS_lseek, fd, offset, whence);
 }
 
 int _gettimeofday(struct timeval *tv, struct timezone *tz) {
