b88940dd458b2a43d2973dc09133399cdc9d7dc5
Guanghui Hu <120L052208@stu.hit.edu.cn>
2023-10-02 21:32:40 +0800

    [navy]: parse a0 ptr to args and envp

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,8 +5,9 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  char *empty[] =  {NULL };
-  environ = empty;
-  exit(main(0, empty, empty));
+  int argc = *(int *)args;
+  char **argv = (char **)((unsigned char *)args + sizeof(int));
+  char **envp = argv + (argc + 1);
+  exit(main(argc, argv, envp));
   assert(0);
 }
